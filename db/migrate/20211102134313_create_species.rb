67c9d998a46d5ac97c0fbbf74f7de9a4d2a14554
Paloma Brandao <brandao.pcg@gmail.com>
2021-11-02 08:43:55 -0500

    Added Species model.

---
+++ db/migrate/20211102134313_create_species.rb
@@ -0,0 +1,17 @@
+class CreateSpecies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :species do |t|
+      t.string :name
+      t.string :classification
+      t.string :designation
+      t.string :average_height
+      t.string :average_lifespan
+      t.string :eye_colors
+      t.string :hair_colors
+      t.string :skin_colors
+      t.string :language
+
+      t.timestamps
+    end
+  end
+end
