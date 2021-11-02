5a489c859a0f9120a40a12b4ca4bf4fbbd38c4eb
Paloma Brandao <brandao.pcg@gmail.com>
2021-11-02 17:53:26 -0500

    Added Planet model

---
+++ db/migrate/20211102223145_create_planets.rb
@@ -0,0 +1,17 @@
+class CreatePlanets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :planets do |t|
+      t.string :name
+      t.string :diameter
+      t.string :rotation_period
+      t.string :orbital_period
+      t.string :gravity
+      t.string :population
+      t.string :climate
+      t.string :terrain
+      t.string :surface_water
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_130742) do
+ActiveRecord::Schema.define(version: 2021_11_02_223145) do
 
   create_table "films", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,34 @@ ActiveRecord::Schema.define(version: 2021_11_02_130742) do
     t.integer "height"
     t.integer "mass"
     t.string "skin_color"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "planets", force: :cascade do |t|
+    t.string "name"
+    t.string "diameter"
+    t.string "rotation_period"
+    t.string "orbital_period"
+    t.string "gravity"
+    t.string "population"
+    t.string "climate"
+    t.string "terrain"
+    t.string "surface_water"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "species", force: :cascade do |t|
+    t.string "name"
+    t.string "classification"
+    t.string "designation"
+    t.string "average_height"
+    t.string "average_lifespan"
+    t.string "eye_colors"
+    t.string "hair_colors"
+    t.string "skin_colors"
+    t.string "language"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
