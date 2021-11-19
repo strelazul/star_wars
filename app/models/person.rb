d3bc640a674444afa7d0f4ae12f7d4324a6788c6
Paloma Brandao <brandao.pcg@gmail.com>
2021-11-18 22:01:07 -0600

    Updating Person relationships.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ApplicationRecord
   has_and_belongs_to_many :films
+  has_and_belongs_to_many :starships
+  has_and_belongs_to_many :vehicles
   has_many :species
   has_one :planets
 end
