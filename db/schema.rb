743b5ca92ec149994263e3777067ca31d1de422c
Paloma Brandao <brandao.pcg@gmail.com>
2021-11-18 23:33:55 -0600

    Revert "Removing Person unnecessary join tables."
    This reverts commit 0ebbbeda3491c6592318315e121cf1ef884feff9.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_119_043_728) do
+ActiveRecord::Schema.define(version: 2021_11_19_043728) do
+
   create_table "films", force: :cascade do |t|
     t.string "title"
     t.integer "episode_id"
@@ -20,6 +21,12 @@ ActiveRecord::Schema.define(version: 20_211_119_043_728) do
     t.date "release_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "people_id"
+    t.integer "species_id"
+    t.integer "planets_id"
+    t.index ["people_id"], name: "index_films_on_people_id"
+    t.index ["planets_id"], name: "index_films_on_planets_id"
+    t.index ["species_id"], name: "index_films_on_species_id"
   end
 
   create_table "films_people", id: false, force: :cascade do |t|
@@ -58,6 +65,20 @@ ActiveRecord::Schema.define(version: 20_211_119_043_728) do
     t.integer "species_id"
     t.index ["planet_id"], name: "index_people_on_planet_id"
     t.index ["species_id"], name: "index_people_on_species_id"
+  end
+
+  create_table "people_planets", id: false, force: :cascade do |t|
+    t.integer "person_id", null: false
+    t.integer "planet_id", null: false
+    t.index ["person_id", "planet_id"], name: "index_people_planets_on_person_id_and_planet_id"
+    t.index ["planet_id", "person_id"], name: "index_people_planets_on_planet_id_and_person_id"
+  end
+
+  create_table "people_species", id: false, force: :cascade do |t|
+    t.integer "person_id", null: false
+    t.integer "species_id", null: false
+    t.index ["person_id", "species_id"], name: "index_people_species_on_person_id_and_species_id"
+    t.index ["species_id", "person_id"], name: "index_people_species_on_species_id_and_person_id"
   end
 
   create_table "planets", force: :cascade do |t|
