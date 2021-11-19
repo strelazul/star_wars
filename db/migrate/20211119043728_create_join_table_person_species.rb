b76209778bc49850217e1be39fbec0ca9f3f7fc3
Paloma Brandao <brandao.pcg@gmail.com>
2021-11-18 23:41:33 -0600

    Fixing conflict.

--- db/migrate/20211119043728_create_join_table_person_species.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTablePersonSpecies < ActiveRecord::Migration[6.1]
-  def change
-    create_join_table :people, :species do |t|
-      t.index [:person_id, :species_id]
-      t.index [:species_id, :person_id]
-    end
-  end
-end
