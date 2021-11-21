8e38d8b7b1d44317632279822f4f0f7b7a0d42b2
Paloma Brandao <brandao.pcg@gmail.com>
2021-11-20 22:44:46 -0600

    Fixied relationship between Person and Species.

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,5 +2,6 @@ class Person < ApplicationRecord
   has_and_belongs_to_many :films
   has_and_belongs_to_many :starships
   has_and_belongs_to_many :vehicles
+  has_and_belongs_to_many :species
   belongs_to :planets
 end
