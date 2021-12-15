d87b6858eaf554e1bbc664f82d1341e667dd8bb4
Paloma Brandao <brandao.pcg@gmail.com>
2021-12-15 14:54:34 -0600

    Installed and configured Boostrap.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "jquery"
+import "@popperjs/core"
+import "bootstrap"
+
+import { Tooltip, Popover, Dropdown } from "bootstrap"
+require("../stylesheets/application.scss")
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,3 +18,20 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("turbolinks:load", () => {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+      return new Tooltip(tooltipTriggerEl)
+  })
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+      return new Popover(popoverTriggerEl)
+  })
+
+  var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+  var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+  })
+ })
